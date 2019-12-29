c55d88c0b10527fff848e7cf01d3e1828c2a239a
Pranav Balaji <balaji.pranav@outlook.in>
2019-12-29 13:37:58 +0530

    Revision 2F - Minimal delays and adverts
    Revision 2F - Includes minimal delays (time.sleep) and TRIVAGO! advert

--- hms1.py
+++ hms1.py
@@ -4,8 +4,16 @@
 # Created for Class - XI Python Project
 # Requires a local SQL database (named delta) with a table (named cust).
 # Database can be not-local, i.e. hosted on the internet; values to be speified for the same in the program.
+# Also includes provision for background-coloured text for sponsor/ partner adverts.
 
 def mainmenu(): #defining a function for the main menu
+    from colorama import init, Fore, Back, Style #color-settings for the partner/sponsor adverts
+    init(convert = True)
+    print(Fore.RED) #red-line to indicate program start
+    print("---------------------------------------------")
+    print(Fore.WHITE)
+    print('A word from our partner: ' + Fore.BLACK + Back.CYAN + 'HOTEL? Trivago!') #Text over here
+    print(Style.RESET_ALL) 
     print("Welcome to the delta Electronics Store!")
     print("Enter: ") 
     print("'1' to GENERATE A BILL")
@@ -42,7 +50,7 @@ def inserter(custid, custname, email): #defining a function to input data into t
 
 while(1): #while (always) true
     mainmenu() #mainmenu
-    time.sleep(1) #for a seamless experience
+    time.sleep(0.34) #for a seamless experience
     decfac = int(input("Enter your choice now: "))
 
     #Bill Mode
@@ -59,17 +67,17 @@ while(1): #while (always) true
         logger.write(dt_string)
         logger.write(" \n")
         abcd1 = 1
-        time.sleep(0.7) #for a seamless experience
+        time.sleep(0.3) #for a seamless experience
         #Values stored in two dictionaries
         data = {"del1":40000, "del2":55000, "del3":67000, "del4":25000, "del5":21000, "del6":14000, "del7":13000, "del8":220000, "del9":4500, "del10":17000, "del11":1200, "del12":3700, "del13":4500, "del14":2200, "del15":700, "del16":2750, "del17":6499, "del18":1499, "del19":799, "del20":27000, "del21":6750, "del22":2100, "del23":1199, "del24":3210, "del25":989, "del26":750, "del27":1700, "del28":600, "del29":2175, "del30":890, "del31":2100, "del32":7158, "del33":597, "del34":347, "del35":500, "del36":300, "del37":1097, "del38":80000, "del39":87900, "del40":23790}
         namie = {"del1":"TV 4K OLED 50", "del2":"TV FHD OLED 50", "del3":"8K QLED 80", "del4":"Redmi K20 PRO", "del5":"Redmi K20", "del6":"Redmi Note 8 PRO", "del7":"POCOPHONE F1", "del8":"Mi MIX ALPHA", "del9":"delta CaptureElite Wireless Headphones", "del10":"delta CaptureElite Noise-Cancelling Wireless Headphones", "del11":"delta CaptureElite Essentials Headphones", "del12":"delta CaptureElite Gaming Headphones", "del13":"delta CaptureElite Truly-Wireless Eadphones", "del14":"delta CaptureElite Neckband-Style Wireless Earphones", "del15":"delta CaptureElite Essentials Earphones", "del16":"delta CaptureElite Gaming Earphones", "del17":"delta CaptureElite 30W Bluetooth Speakers", "del18":"delta CaptureElite 10W Bluetooth Speakers", "del19":"delta CaptureElite Essentials Bluetooth Speaker", "del20":"delta CaptureElite ULTRA Home Theatre", "del21":"delta CaptureElite Essentials Home Theatre", "del22":"delta CaptureElite Wired Speaker - 5.1", "del23":"delta CaptureElite Essentials Wired Speaker - STEREO", "del24":"delta Polowski Tactical SHERPAELITE Power Bank 30000mah", "del25":"delta Polowski Tactical Essentials Power Bank 10000mah", "del26":"delta Polowski Tactical Essentials Mouse", "del27":"delta Polowski Tactical RGB Gaming Mouse", "del28":"delta Polowski Tactical Essentials Keyboard", "del29":"delta Polowski Tactical RGB Gaming Keyboard", "del30":"delta Polowski Tactical SHERPAELITE Flashlight", "del31":"deltaNetworking Wi-Fi Router AX17", "del32":"deltaNetworking SHERPAELITE Mesh Wi-Fi Router", "del33":"deltaSupport 120W Laptop Adapter", "del34":"deltaSupport 60W Laptop Adapter", "del35":"deltaSupport Phone Case", "del36":"deltaSupport Essentials Phone Charger 10W", "del37":"deltaSupport SHERPAELITE Phone Charger 30W", "del38":"deltaCiccadella Gaming Laptop", "del39":"deltaCiccadella Content Creator's Laptop", "del40":"deltaCiccadella Student's Laptop"}
         numfac = int(input("Enter the number of items: "))
-        time.sleep(1) #for a seamless experience
+        time.sleep(0.34) #for a seamless experience
         afac = 0
         billiemaster = 0 #variable for totalling the price
         while(afac!=numfac):
             item = input("Enter the item code: ")
-            time.sleep(1) #for a seamless experience
+            time.sleep(0.3) #for a seamless experience
             if item in data:
                 billiemaster+=data[item]
                 print("Product purchased: ", namie[item], " costing: ", data[item])
@@ -84,12 +92,12 @@ while(1): #while (always) true
             afac+=1
         tax = int(input("Enter the net tax %: "))
         print(tax,"% NET TAX - Incoicing!")
-        time.sleep(1) #for a seamless experience
+        time.sleep(0.4) #for a seamless experience
         discount = int(input("Enter the discount %: "))
         print(discount,"% NET DISCOUNT - Invoicing!")
         time.sleep(0.4) #for a seamless experience
         print("Please Wait....... Billing.......")
-        time.sleep(1.3) #for a seamless experience
+        time.sleep(0.67) #for a seamless experience
         tota = (((tax/100)*billiemaster)+billiemaster)
         total = tota-((discount/100)*tota)
         print("BILL NUMBER: ", abcd1, "; the total bill is: ", total)
@@ -98,12 +106,12 @@ while(1): #while (always) true
         logger.write("\n")
         abcd1+=1
         afac+=1
-        time.sleep(2) #for a seamless experience
+        time.sleep(1.67) #for a seamless experience
         print()
         print()
     #Register Customer
     elif decfac == 2:
-        print("Loading server connection....... ") #SQL connection prompt
+        print("Connecting to server....... Please wait...") #SQL connection prompt
         time.sleep(0.4) #for a seamless experience
         custid = input("Enter the customer's customer ID: ")
         custname = input("Enter the customer's name: ")
@@ -114,8 +122,9 @@ while(1): #while (always) true
     #VIEW ALL CUSTOMERS
     elif decfac == 3:
         print()
-        print("The registered customers are: ")
+        print("Connecting to server....... Please wait...") #SQL connection prompt
         time.sleep(0.7) #for a seamless experience
+        print("The registered customers are: ")
         #Re-writing to refresh connection
         conn = mysql.connector.connect(host='localhost', database='delta', user='root', password='shieldlogmein')
         cursor = conn.cursor()
@@ -129,6 +138,7 @@ while(1): #while (always) true
         conn.close()
         print()
         print()
+        time.sleep(2) #delay for easy-table viewing
 
     #View Generated Bills
     elif decfac == 4:
@@ -137,8 +147,9 @@ while(1): #while (always) true
         if passw == "root":
                 time.sleep(1) #for a seamless experience
                 print("Authorization Succesfull! ")
+                time.sleep(0.4)
                 print("Opening sales log externally:: ")
-                time.sleep(0.6)
+                time.sleep(0.2) #for a seamless experience 
                 logger.close() #to change file access modes 
                 logger = open("log.txt","r+")  
                 # Uncomment the below lines if the program has to be modified to show the records in the shell itself and not externally
